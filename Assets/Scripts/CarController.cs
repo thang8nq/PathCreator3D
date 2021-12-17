ae87d8cf80d2ade5d0dadd07de350cb1c83446db
Nguyen Quang Thang <thang.nguyenquang@gameloft.com>
2021-12-17 15:04:35 +0700

    *racing 1 round (setup destination) EndOfPathInstruction=stop

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,18 +6,23 @@ using PathCreation;
 public class CarController : MonoBehaviour
 {
     [SerializeField] PathCreator pathCreator;
+    [SerializeField] EndOfPathInstruction destination;
     [SerializeField] float _speed;
     float _distanceTravelled; 
 
     void Start()
     {
-        
+        Debug.Log("start pos: " + pathCreator.path.GetPoint(0));
+        Debug.Log("end pos: " + pathCreator.path.GetPoint(1));
+        Debug.Log("rad pos: " + pathCreator.path.GetPoint(3));
     }
 
     void Update()
     {
         _distanceTravelled += _speed * Time.deltaTime;
-        transform.position = pathCreator.path.GetPointAtDistance(_distanceTravelled);
-        transform.rotation = pathCreator.path.GetRotationAtDistance(_distanceTravelled);
+        transform.position = pathCreator.path.GetPointAtDistance(_distanceTravelled, destination);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(_distanceTravelled, destination);
     }
+
+
 }
